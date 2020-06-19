72afff87a99533a2b9a1653941dd392a756765a8
JulianCode <hijuliansosa@protonmail.com>
2020-06-19 00:04:58 -0500

    Creando la App y Primeros Ajustes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <header>
+      <h1>Presupuesto de gasto Semanal</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
